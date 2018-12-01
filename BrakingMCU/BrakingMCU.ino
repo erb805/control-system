1f9e4fe7bb11b9da879effa0bcc12648766ed8ae
Erik Bruenner <ebruenne@calpoly.edu>
2018-12-01 14:10:02 -0800

    Update BrakingMCU.ino

---
+++ BrakingMCU/BrakingMCU.ino
@@ -0,0 +1,3 @@
+//We discussed using MSP432 MCU instead of an Arduino uno for the Braking/Tensioning MCU
+//This would mean we would have two separate i2c networks connecting this MCU to the RPI, and the MCU
+//to the programmable regulator. (Slave to RPI, master to regulator)
